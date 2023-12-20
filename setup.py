d6b0dee262e2d1b3098a4ca5eb9fa00931fd6ee3
roxyash <roxyash@mail.ru>
2023-12-20 22:08:36 +0600

    fix: setup.py file

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import setup, find_packages
 setup(
     name="webscrapper_proto",
     version="0.1",
-    packages=find_packages(),
+    packages=find_packages(include=['gen*', 'gen.python*', 'gen.python.scrapper*']),
 )
